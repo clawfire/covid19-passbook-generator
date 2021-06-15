58f639842f08202a33dbbb540d9d55f000656db4
Thibault Milan <hello@thibaultmilan.com>
2021-06-15 21:54:42 +0200

    Start decoding the qr-code data. Trim the header. Decode the binary.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
 import Handlebars from 'handlebars';
 import QrScanner from 'qr-scanner';
+const b45 = require('base45-js');
+const zlib = require('pako');
+import cbor from 'cbor-js';
 
 import template from './template.json';
+
+
+// function decodeToUtf8String(utf8StringArg) {
+//
+//     let data = b45.decode(utf8StringArg);
+//
+//     var str = "";
+//     var count = data.length;
+//     for (let i = 0; i < count; ++i)
+//         str += String.fromCharCode(data[i]);
+//
+//     return str;
+// }
 
 window.addEventListener('load', function() {
 
@@ -19,10 +35,42 @@ window.addEventListener('load', function() {
     // on créé un element de scanner
     const scanner = new QrScanner(video, result => decode(result));
 
-    function decode(result) {
+    function decode(data) {
+        // destroy the scanner, we gonna need memory
         scanner.destroy();
-        template.barcode.message = result;
-        console.log(template);
+        // What we read
+        console.log("Data from the qr code %o", data);
+        // Add it as a QRcode in the template
+        template.barcode.message = data;
+        // Strip off the HC1 header if present
+        if (data.startsWith("HC1")) {
+            data = data.substring(3)
+            if (data.startsWith(':')) {
+                data = data.substring(1);
+            } else {
+                console.warn("Warning: unsafe HC1: header");
+            }
+        } else {
+            console.warn("Warning: no HC1: header");
+        }
+        // Now decoding
+        data = b45.decode(data);
+        // Zlib magic headers:
+        // 78 01 - No Compression/low
+        // 78 9C - Default Compression
+        // 78 DA - Best Compression
+        //
+        if (data[0] == 0x78) {
+            console.info("Deflating ...")
+            data = zlib.inflate(data)
+        }
+        console.log("Decoded Uint8Array data %o", data);
+        data = String.fromCharCode.apply(null, data);
+        console.log("Decoded string data %o", data);
+        console.log(cbor.encode({
+            Hello: "World"
+        }));
+        data = cbor.decode(data);
     }
 
     // on démarre le scan
